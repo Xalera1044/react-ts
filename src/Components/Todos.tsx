f7cb53d155b031af62cb5ed4bf20487129303473
AbbasNazarov <abbas.nazarov@gmail.com>
2023-03-19 23:47:51 +0400

    added delete

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -2,11 +2,11 @@ import TodoItem from './Todoitem';
 import Todo from '../Models/todo';
 import React from 'react';
 import classes from './Todos.module.css'
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: () => void }> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo} />
       ))}
     </ul>
   );
